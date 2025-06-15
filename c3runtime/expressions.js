51319556878d3b0052fa3f1e42f56797b6710677
FONGKA2 <FONGKA2@GMAIL.COM>
2025-06-15 13:55:08 +0800

    Specify default language

--- c3runtime/expressions.js
+++ c3runtime/expressions.js
@@ -1,4 +1,28 @@
 "use strict";
 
 C3.Plugins.Eponesh_DexieQuery.Exps = {
+    LastPath()
+    {
+        return this._lastPath || "";
+    },
+
+    LastFile()
+    {
+        return this._lastFile || "";
+    },
+
+    GetDefaultNeedVerify()
+    {
+        return this._defaultNeedVerify|0;
+    },
+
+    GetEnableLog()
+    {
+        return this._enableLog ? 1 : 0;
+    },
+
+    GetQuerySuccess()
+    {
+        return this._querySuccess ? 1 : 0;
+    }
 };
