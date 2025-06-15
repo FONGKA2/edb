4e15ea1faf841e695e34481e192bd375740b05d6
FONGKA2 <FONGKA2@GMAIL.COM>
2025-06-15 15:28:12 +0800

    fix: set language tag

--- c3runtime/conditions.js
+++ c3runtime/conditions.js
@@ -1,4 +1,27 @@
 "use strict";
 
 C3.Plugins.Eponesh_DexieQuery.Cnds = {
+    OnQuerySuccess() {},
+    OnQueryFailed() {},
+    OnVerifyFailed() {},
+
+    IsLastQuerySuccess()
+    {
+        return !!this._querySuccess;
+    },
+
+    IsLogEnabled()
+    {
+        return !!this._enableLog;
+    },
+
+    ComparePath(path)
+    {
+        return this._lastPath === path;
+    },
+
+    HasResult()
+    {
+        return !!this._lastFile;
+    }
 };
--- c3runtime/instance.js
+++ c3runtime/instance.js
@@ -2,6 +2,40 @@
 
 C3.Plugins.Eponesh_DexieQuery.Instance = class extends globalThis.ISDKInstanceBase {
     Release() {}
-    OnCreate() {}
-    OnPropertyChanged(id, value) {}
+
+    OnCreate()
+    {
+        this._db = null;
+        this._enableLog = false;
+        this._defaultNeedVerify = 0;
+        this._lastPath = "";
+        this._lastFile = "";
+        this._querySuccess = false;
+    }
+
+    OnPropertyChanged(id, value)
+    {
+        if (id === "enable-log")
+            this._enableLog = !!value;
+    }
+
+    _log(msg)
+    {
+        if (this._enableLog)
+            console.log(`[DexieQuery] ${msg}`);
+    }
+
+    _fromHex(hex)
+    {
+        const bytes = new Uint8Array(hex.length / 2);
+        for (let i = 0; i < bytes.length; i++)
+            bytes[i] = parseInt(hex.substr(i * 2, 2), 16);
+        return bytes;
+    }
+
+    _trigger(cnd)
+    {
+        if (this._runtime && this._runtime.Trigger)
+            this._runtime.Trigger(cnd, this);
+    }
 };
